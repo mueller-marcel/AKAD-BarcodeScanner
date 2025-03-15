a86cc67425e95541f0dbb3752083077ffb384a39
Marcel Müller <mueller.marcel69@gmail.com>
2025-03-15 11:16:02 +0100

    Refactored comment and linebreak

--- barcode/scanner.py
+++ barcode/scanner.py
@@ -1,6 +1,5 @@
 import cv2
 
-
 def scan_barcode(image_path: str, text_file_path: str) -> bool:
     """
     Retrieves the barcode from the image_path and compares it with the barcode text from
@@ -17,7 +16,7 @@ def scan_barcode(image_path: str, text_file_path: str) -> bool:
     # Create a binary image
     _, binary_image = cv2.threshold(image, 128, 255, cv2.THRESH_BINARY)
 
-    # Ergebnisse anzeigen
+    # Show image
     cv2.imshow("Detected", binary_image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
