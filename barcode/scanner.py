a1f9b533cd30e500ffb03099f1e18b354f0ee011
Marcel Müller <mueller.marcel69@gmail.com>
2025-03-15 11:22:18 +0100

    Refactored the preprocess code into a separate function

--- barcode/scanner.py
+++ barcode/scanner.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 def scan_barcode(image_path: str, text_file_path: str) -> bool:
     """
@@ -13,6 +14,18 @@ def scan_barcode(image_path: str, text_file_path: str) -> bool:
     # Load the image using grayscale
     image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
 
+    # Preprocess the image
+    image = preprocess_image(image)
+
+    return True
+
+def preprocess_image(image: np.ndarray) -> np.ndarray:
+    """
+    Preprocesses the image and returns the preprocessed image. Extracts the barcode from the image.
+    :param image: The image to preprocess
+    :return: The preprocessed image of the barcode only
+    """
+
     # Create a binary image
     _, binary_image = cv2.threshold(image, 128, 255, cv2.THRESH_BINARY)
 
@@ -21,4 +34,4 @@ def scan_barcode(image_path: str, text_file_path: str) -> bool:
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-    return True
+    return image
